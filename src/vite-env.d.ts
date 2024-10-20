ade19f1c3e6c1e471116fbcfc0d4c2dbd4bb8820
Alexandra Dubrivna <110938636+SanyaKiss@users.noreply.github.com>
2024-10-20 21:39:52 +0300

    Update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,1 @@
-VITE_API_KEY = 'AIzaSyB11p8j_ubEwPhiP039YKXz_Zy4WtUk3sE'
-VITE_AUTH_DOMAIN = 'react-redux-shop-eb24e.firebaseapp.com'
-VITE_PROJECT_ID = 'react-redux-shop-eb24e'
-VITE_STORAGE_BUCKET = 'react-redux-shop-eb24e.appspot.com'
-VITE_MESSAGING_SENDER_ID = '945361275025'
-VITE_APP_ID = '1:945361275025:web:5e46824c7bbfe6a3819730'
+
